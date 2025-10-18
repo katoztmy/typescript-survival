25f2d6c8b0fc121e0d7df0137e6738b916738a28
katoztmy <kato.eng.dao@gmail.com>
2025-10-18 22:16:35 +0900

    分割代入とか

--- src/typescript_kiso/20251018/index.ts
+++ src/typescript_kiso/20251018/index.ts
@@ -64,3 +64,60 @@ console.log(Math.max(...numbers)); // 100
 const xnumbers = [3, 6, 9, 12];
 const [sone, two, three] = xnumbers;
 console.log(sone, two, three);
+
+// ネストした配列の分割代入
+const xnumbers2 = [
+  [1, 2],
+  [3, 4],
+];
+
+const [[oneX, twoX], [threeX]] = xnumbers2;
+console.log(oneX, twoX, threeX);
+
+const color = { r: 9, g: 10, b: 112, z: 123, ttt: 300, a: 1, bbb: 200 };
+const { z, ttt, bbb } = color;
+console.log(z, ttt, bbb);
+
+// オプショナルチェーン
+const book = undefined;
+// bookはundefinedなので下記の条件文の中には入らない。その場合、bookの型はnever型になる。
+if (book) {
+  const page = book.page;
+}
+
+const book2 = undefined;
+const title = book2?.title;
+console.log(title);
+
+const foo = { a: 1 };
+const sdate = new Date();
+const arr = [1, 2, 3];
+console.log(foo.hi, sdate.hi, arr.hi);
+
+Object.prototype.hi = "hi";
+console.log(foo.hi, sdate.hi, arr.hi);
+
+for (const key in foo) {
+  // fooのプロパティはaとprototype.hiで追加されたhiの二つが入ってしまう
+  console.log(key);
+}
+
+const foo2 = { a: 1, b: 2, c: 3 };
+
+// Object.entries()の結果を確認
+console.log("Object.entries(foo2):", Object.entries(foo2));
+// [["a", 1], ["b", 2], ["c", 3]] が出力される
+
+// 正しい分割代入：[key, value]の順序
+for (const [key, value] of Object.entries(foo2)) {
+  console.log(`key: ${key}, value: ${value}`);
+  // key: a, value: 1
+  // key: b, value: 2
+  // key: c, value: 3 の順で出力される
+}
+
+// 間違った分割代入の例（コメントのコード）
+// for (const [value, key] of Object.entries(foo2)) {
+//   console.log(value, key);
+//   // 実際は key value の順で出力される（変数名と実際の内容が逆）
+// }
